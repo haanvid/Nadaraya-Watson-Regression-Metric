defae85e137dbfaf58bc4c28de235cdd778e6ba4
haanvid <haanvidlee@gmail.com>
2019-11-05 21:21:14 +0900

    fist commit

--- Slice Location/getScaledKernelVal.m
+++
@@ -1,7 +0,0 @@
-function [kernelOutputs, logKernelOutputs, logscale] = getScaledKernelVal(logkernelvals)
-% scale kernel outputs
-% logkernelvals: tstdatanum x trdatanum
-
-logscale = max(logkernelvals, [], 2);
-logKernelOutputs = logkernelvals - logscale*ones(1,size(logkernelvals,2));
-kernelOutputs = exp(logKernelOutputs);
